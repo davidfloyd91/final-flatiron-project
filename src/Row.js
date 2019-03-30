4536d26546ef3844e9c1eacbf3a2b1c3387c8d2b
davidfloyd91 <davidfloyd91@gmail.com>
2019-03-30 14:20:22 -0400

    table state is responsive to cell changes

--- src/Row.js
+++ src/Row.js
@@ -1,70 +1,36 @@
-// eslint-disable-next-line
-import React from 'react'
-import PropTypes from 'prop-types'
-import Cell from './Cell'
+import React, { Component, Fragment } from 'react';
+import Cell from './Cell';
 
-/**
- * Row manages a single row of the table
- */
-const Row = (props) => {
-  const cells = []
-  const y = props.y
-  for (let x = 0; x < props.x; x += 1) {
-    cells.push(
-      <Cell
-        key={`${x}-${y}`}
-        y={y}
-        x={x}
-        onChangedValue={props.handleChangedCell}
-        updateCells={props.updateCells}
-        value={props.rowData[x] || ''}
-        executeFormula={props.executeFormula}
-      />,
-    )
-  }
-  return (
-    <div>
-      {cells}
-    </div>
-  )
-}
+export default class Row extends Component {
+  newValue = (value, x, y) => {
+    this.props.newValue(value, x, y);
+  };
 
-Row.propTypes = {
-  /**
-   * Function called when a cell of the row changes
-   * its value
-   */
-  handleChangedCell: PropTypes.func.isRequired,
+  renderCells = () => {
+    let yArray = [];
+    for (let y = 0; y < this.props.y; y++) {
+      yArray.push(y);
+    };
 
-  /**
-   * Function called when a cell of the row needs
-   * a formula recalculation
-   */
-  executeFormula: PropTypes.func.isRequired,
+    return yArray.map(y => {
+      let keyId = `${this.props.x + 1}-${y + 1}`;
 
-  /**
-   * Function called when a cell is refreshed and requires
-   * an update of the others
-   */
-  updateCells: PropTypes.func.isRequired,
+      return (
+        <Cell
+          key={keyId}
+          x={this.props.x + 1}
+          y={y + 1}
+          newValue={this.newValue}
+        />
+      );
+    });
+  };
 
-  /**
-   * The number of columns of the table, used to know
-   * how many cells to add
-   */
-  x: PropTypes.number.isRequired,
-
-  /**
-   * The identifier value of the row
-   */
-  y: PropTypes.number.isRequired,
-
-  /**
-   * The values of the cells in the row
-   */
-  rowData: PropTypes.shape({
-    string: PropTypes.string,
-  }).isRequired,
-}
-
-export default Row
+  render() {
+    return (
+      <Fragment>
+        {this.renderCells()}
+      </Fragment>
+    );
+  };
+};
--- src/Table.js
+++ src/Table.js
@@ -1,196 +1,67 @@
-// eslint-disable-next-line
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Parser as FormulaParser } from 'hot-formula-parser'
-import Row from './Row'
+import React, { Component, Fragment } from 'react';
+import Row from './Row';
+import './App.css';
 
-/**
- * Table creates a table with x rows and y columns
- */
-export default class Table extends React.Component {
-  constructor(props) {
-    super(props)
+export default class Table extends Component {
+  state = {
+    grid: {},
+  };
 
-    this.state = {
-      data: {},
-    }
+  componentDidMount() {
+    let row = [];
+    let grid = {};
 
-    this.tableIdentifier = `tableData-${props.id}`
+    for (let y = 0; y < this.props.y; y++) {
+      row = [...row, {value: ''}];
+    };
 
-    // Initialize the formula parser on demand
-    this.parser = new FormulaParser()
+    for (let x = 0; x < this.props.x; x++) {
+      grid = {...grid, [x + 1]: row};
+    };
 
-    // When a formula contains a cell value, this event lets us
-    // hook and return an error value if necessary
-    this.parser.on('callCellValue', (cellCoord, done) => {
-      const x = cellCoord.column.index + 1
-      const y = cellCoord.row.index + 1
+    this.setState({ grid });
+  };
 
-      // Check if I have that coordinates tuple in the table range
-      if (x > this.props.x || y > this.props.y) {
-        throw this.parser.Error(this.parser.ERROR_NOT_AVAILABLE)
-      }
+  renderRows = () => {
+    let xArray = [];
 
-      // Check that the cell is not self referencing
-      if (this.parser.cell.x === x && this.parser.cell.y === y) {
-        throw this.parser.Error(this.parser.ERROR_REF)
-      }
+    for (let x = 0; x < this.props.x; x++) {
+      xArray.push(x);
+    };
 
-      if (!this.state.data[y] || !this.state.data[y][x]) {
-        return done('')
-      }
+    return xArray.map(x => {
+      return (
+        <div className='row' key={x}>
+          <Row
+            key={x}
+            id={x}
+            x={x}
+            y={this.props.y}
+            newValue={this.newValue}
+          />
+        </div>
+      );
+    });
+  };
 
-      // All fine
-      return done(this.state.data[y][x])
-    })
-
-    // When a formula contains a range value, this event lets us
-    // hook and return an error value if necessary
-    this.parser.on('callRangeValue', (startCellCoord, endCellCoord, done) => {
-      const sx = startCellCoord.column.index + 1
-      const sy = startCellCoord.row.index + 1
-      const ex = endCellCoord.column.index + 1
-      const ey = endCellCoord.row.index + 1
-      const fragment = []
-
-      for (let y = sy; y <= ey; y += 1) {
-        const row = this.state.data[y]
-        if (!row) {
-          continue
-        }
-
-        const colFragment = []
-
-        for (let x = sx; x <= ex; x += 1) {
-          let value = row[x]
-          if (!value) {
-            value = ''
-          }
-
-          if (value.slice(0, 1) === '=') {
-            const res = this.executeFormula({ x, y }, value.slice(1))
-            if (res.error) {
-              throw this.parser.Error(res.error)
-            }
-            value = res.result
-          }
-
-          colFragment.push(value)
-        }
-        fragment.push(colFragment)
-      }
-
-      if (fragment) {
-        done(fragment)
-      }
-    })
-  }
-
-  /**
-   * Initialize the sate from the localstorage, if found
-   */
-  componentWillMount() {
-    if (this.props.saveToLocalStorage && window && window.localStorage) {
-      const data = window.localStorage.getItem(this.tableIdentifier)
-      if (data) {
-        // antipattern
-        this.setState({ data: JSON.parse(data) })
-      }
-    }
-  }
-
-  /**
-   * Force an update of the component
-   */
-  updateCells = () => {
-    this.forceUpdate()
-  }
-
-  /**
-   * Executes the formula on the `value` usign the FormulaParser object
-   */
-  executeFormula = (cell, value) => {
-    this.parser.cell = cell
-    let res = this.parser.parse(value)
-    if (res.error != null) {
-      return res // tip: returning `res.error` shows more details
-    }
-    if (res.result.toString() === '') {
-      return res
-    }
-    if (res.result.toString().slice(0, 1) === '=') {
-      // formula points to formula
-      res = this.executeFormula(cell, res.result.slice(1))
-    }
-
-    return res
-  }
-
-  /**
-   * Handles changing a cell, stores the new data state and stores into
-   * local storage
-   */
-  handleChangedCell = ({ x, y }, value) => {
-    const modifiedData = Object.assign({}, this.state.data)
-    if (!modifiedData[y]) modifiedData[y] = {}
-    modifiedData[y][x] = value
-    this.setState({ data: modifiedData })
-
-    if (this.props.saveToLocalStorage && window && window.localStorage) {
-      window.localStorage.setItem(this.tableIdentifier, JSON.stringify(modifiedData))
-    }
-  }
+  newValue = (value, x, y) => {
+    this.setState({
+      grid: {
+        ...this.state.grid,
+        [x]: [
+          ...this.state.grid[x].slice(0, y - 1),
+          {value},
+          ...this.state.grid[x].slice(y)
+        ]
+      },
+    });
+  };
 
   render() {
-    const rows = []
-
-    for (let y = 0; y < this.props.y + 1; y += 1) {
-      const rowData = this.state.data[y] || {}
-      rows.push(
-        <Row
-          handleChangedCell={this.handleChangedCell}
-          executeFormula={this.executeFormula}
-          updateCells={this.updateCells}
-          key={y}
-          y={y}
-          x={this.props.x + 1}
-          rowData={rowData}
-        />,
-      )
-    }
     return (
-      <div>
-        {rows}
-      </div>
-    )
-  }
-}
-
-Table.propTypes = {
-  /**
-   * The number of columns of the table
-   */
-  x: PropTypes.number.isRequired,
-
-  /**
-   * The number of rows of the table
-   */
-  y: PropTypes.number.isRequired,
-
-  /**
-   * An optional ID for the table, useful to use
-   * multiple tables and store into localStorage
-   */
-  id: PropTypes.string,
-
-  /**
-   * If enabled, saves the table state to the localStorage
-   * Otherwise the table is refreshed on every save
-   */
-  saveToLocalStorage: PropTypes.bool,
-}
-
-Table.defaultProps = {
-  saveToLocalStorage: true,
-  id: 'default',
-}
+      <Fragment>
+        {this.renderRows()}
+      </Fragment>
+    );
+  };
+};
