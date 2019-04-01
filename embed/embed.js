050b510e5b73780f623cc46fe4e6ec56e2ecba76
davidfloyd91 <davidfloyd91@gmail.com>
2019-04-01 09:35:40 -0400

    basic display via script tag enabled with chartjs dummy code from blog

---
+++ embed/embed.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// this is just example code lifted from https://blog.bitsrc.io/customizing-chart-js-in-react-2199fa81530a to test the ability to embed via script tags
+
+class LineGraph extends React.Component {
+    chartRef = React.createRef();
+
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+
+        new Chart(myChartRef, {
+            type: "line",
+            data: {
+                //Bring in data
+                labels: ["Jan", "Feb", "March"],
+                datasets: [
+                    {
+                        label: "Sales",
+                        data: [86, 67, 91],
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
+    }
+    render() {
+        return (
+            <div>
+                <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
+            </div>
+        )
+    }
+}
+
+let domContainer = document.querySelector('#embed_container');
+ReactDOM.render(<LineGraph />, domContainer);
