e0c7b5e36ccc52f6a1158a3583546f02108714f2
davidfloyd91 <davidfloyd91@gmail.com>
2019-04-05 15:09:15 -0400

    cards in dashboard don't look so trash

--- src/ChartPreview.js
+++ src/ChartPreview.js
@@ -5,16 +5,20 @@ export default class ChartPreview extends Component {
   render() {
     return (
       <div className='card' onClick={() => this.props.showChart(this.props.chartId)}>
-        <h5>{this.props.chart.data.options.title.text}</h5>
-        {
-          this.props.chart.data.type === 'line'
-          ? <img className='previewImg' width='150px' src='/assets/linePreview.png' />
-          : this.props.chart.data.type === 'bar'
-          ? <img className='previewImg' width='180px' src='/assets/barPreview.png' />
-          : this.props.chart.data.type === 'pie'
-          ? <img className='previewImg' width='180px' src='/assets/piePreview.png' />
-          : null
-        }
+        <div className='previewHeader'>
+          <h5>{this.props.chart.data.options.title.text}</h5>
+        </div>
+        <div className='previewImgContainer'>
+          {
+            this.props.chart.data.type === 'line'
+            ? <img className='previewImg' src='/assets/linePreview.png' />
+            : this.props.chart.data.type === 'bar'
+            ? <img className='previewImg' src='/assets/barPreview.png' />
+            : this.props.chart.data.type === 'pie'
+            ? <img className='previewImg' src='/assets/piePreview.png' />
+            : null
+          }
+        </div>
       </div>
     );
   };
