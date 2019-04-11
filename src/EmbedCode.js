d748d6671c1dfeae6cd837c1c7b319fc7851a133
davidfloyd91 <davidfloyd91@gmail.com>
2019-04-11 16:34:41 -0400

    closing branch

--- src/EmbedCode.js
+++ src/EmbedCode.js
@@ -12,21 +12,11 @@ class EmbedCode extends Component {
   render() {
     if (this.data()) {
       return (
-        <div>
-          <p>
-          &lt;iframe srcdoc="
-            &lt;div id='embed_container'&gt;&lt;/div&gt;
-            &lt;script src='https://unpkg.com/react@16/umd/react.development.js' crossorigin&gt;&lt;/script&gt;
-            &lt;script src='https://unpkg.com/react-dom@16/umd/react-dom.development.js' crossorigin&gt;&lt;/script&gt;
-            &lt;script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js' crossorigin&gt;&lt;/script&gt;
-
-            &lt;canvas id='bar-chart' width='1000px' height='800px'&gt;&lt;/canvas&gt;
-            &lt;script&gt;
-              new Chart(document.getElementById('bar-chart'), {this.data()});
-            &lt;/script&gt;" width="1000px" height="800px"&gt;
-          &lt;/iframe&gt;
-          </p>
-        </div>
+        <div className='embedCode'>
+          <code>
+          &lt;iframe srcdoc="&lt;div id='embed_container'&gt;&lt;/div&gt;&lt;script src='https://unpkg.com/react@16/umd/react.development.js' crossorigin&gt;&lt;/script&gt;&lt;script src='https://unpkg.com/react-dom@16/umd/react-dom.development.js' crossorigin&gt;&lt;/script&gt;&lt;script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js' crossorigin&gt;&lt;/script&gt;&lt;canvas id='bar-chart' width='1000px' height='800px'&gt;&lt;/canvas&gt;&lt;script&gt;new Chart(document.getElementById('bar-chart'),{this.data()});&lt;/script&gt;" width="1000px" height="800px"&gt;&lt;/iframe&gt;
+        </code>
+      </div>
       );
     } else {
       return null;
