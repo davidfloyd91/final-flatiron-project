c77af769553ee2028269437040a5244cf1098d0d
davidfloyd91 <davidfloyd91@gmail.com>
2019-04-11 12:31:20 -0400

    it works?

--- src/EmbedCode.js
+++ src/EmbedCode.js
@@ -1,11 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+let width, height;
 
 class EmbedCode extends Component {
   data = () => {
-    return JSON.stringify(this.props.chart.data).replace(/\"/g, '\'');
+    return JSON.stringify(this.props.chart.data).replace(/\"/g, '\'').replace('_datasets', 'datasets');
   };
-
 
   render() {
     return (
@@ -18,10 +18,10 @@ class EmbedCode extends Component {
             &lt;script src='https://unpkg.com/react-dom@16/umd/react-dom.development.js' crossorigin&gt;&lt;/script&gt;
             &lt;script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js' crossorigin&gt;&lt;/script&gt;
 
-            &lt;canvas id='bar-chart' width='800' height='450'&gt;&lt;/canvas&gt;
+            &lt;canvas id='bar-chart' width='1000px' height='800px'&gt;&lt;/canvas&gt;
             &lt;script&gt;
             new Chart(document.getElementById('bar-chart'), {this.data()});
-        &lt;/script&gt;"
+        &lt;/script&gt;" width="1000px" height="800px"&gt;
         &lt;/iframe&gt;
         </p>
       </div>
