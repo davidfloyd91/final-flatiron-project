80b8c002b4badbcf146c008952c2c9f9c6b5e6f1
davidfloyd91 <davidfloyd91@gmail.com>
2019-04-11 22:17:26 -0400

    copying embed code onClick

--- src/EmbedCode.js
+++ src/EmbedCode.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+let copyData;
 
 class EmbedCode extends Component {
+  embedRef = React.createRef();
+
   data = () => {
     if (this.props.chart) {
       let data = this.props.chart.data;
@@ -9,14 +12,20 @@ class EmbedCode extends Component {
     };
   };
 
+  handleClick = () => {
+    const embed = this.embedRef;
+    embed.select();
+    document.execCommand('copy');
+  };
+
   render() {
     if (this.data()) {
+      let showData = `&lt;iframe srcdoc="&lt;div id='embed_container'&gt;&lt;/div&gt;&lt;script src='https://unpkg.com/react@16/umd/react.development.js' crossorigin&gt;&lt;/script&gt;&lt;script src='https://unpkg.com/react-dom@16/umd/react-dom.development.js' crossorigin&gt;&lt;/script&gt;&lt;script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js' crossorigin&gt;&lt;/script&gt;&lt;canvas id='bar-chart' width='1000px' height='800px'&gt;&lt;/canvas&gt;&lt;script&gt;new Chart(document.getElementById('bar-chart'),${this.data()});&lt;/script&gt;" width="1000px" height="800px"&gt;&lt;/iframe&gt;`
+
+      copyData = showData.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+
       return (
-        <div className='embedCode'>
-          <code>
-          &lt;iframe srcdoc="&lt;div id='embed_container'&gt;&lt;/div&gt;&lt;script src='https://unpkg.com/react@16/umd/react.development.js' crossorigin&gt;&lt;/script&gt;&lt;script src='https://unpkg.com/react-dom@16/umd/react-dom.development.js' crossorigin&gt;&lt;/script&gt;&lt;script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js' crossorigin&gt;&lt;/script&gt;&lt;canvas id='bar-chart' width='1000px' height='800px'&gt;&lt;/canvas&gt;&lt;script&gt;new Chart(document.getElementById('bar-chart'),{this.data()});&lt;/script&gt;" width="1000px" height="800px"&gt;&lt;/iframe&gt;
-        </code>
-      </div>
+        <textarea className='embedCode' value={copyData} onClick={this.handleClick} ref={code => {this.embedRef = code;}} />
       );
     } else {
       return null;
