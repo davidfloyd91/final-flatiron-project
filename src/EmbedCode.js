776be64c8c6677bbf56c8acf8ae2a12b6122433e
davidfloyd91 <davidfloyd91@gmail.com>
2019-04-11 23:05:16 -0400

    added some words so the user knows what's going on

--- src/EmbedCode.js
+++ src/EmbedCode.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 
 class EmbedCode extends Component {
@@ -21,7 +21,10 @@ class EmbedCode extends Component {
       const copyData = `<iframe srcdoc="<div id='embed_container'></div><script src='https://unpkg.com/react@16/umd/react.development.js' crossorigin></script><script src='https://unpkg.com/react-dom@16/umd/react-dom.development.js' crossorigin></script><script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js' crossorigin></script><canvas id='bar-chart' width='1000px' height='800px'></canvas><script>new Chart(document.getElementById('bar-chart'),${this.data()});</script>" width="1000px" height="800px"></iframe>`
 
       return (
-        <textarea className='embedCode' value={copyData} onClick={this.handleClick} ref={code => {this.embedRef = code;}} />
+        <Fragment>
+          <h5>Click below to copy your chart's embed code:</h5>
+          <textarea className='embedCode' value={copyData} onClick={this.handleClick} ref={code => {this.embedRef = code;}} />
+        </Fragment>
       );
     } else {
       return null;
