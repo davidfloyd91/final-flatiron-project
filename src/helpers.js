1782bee47713a30461675be7f5dcbbc39549a5b4
davidfloyd91 <davidfloyd91@gmail.com>
2019-04-25 16:23:05 -0400

    changed url back to heroku

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
-// export const url = 'https://salp-api.herokuapp.com';
-export const url = 'http://localhost:3000';
+export const url = 'https://salp-api.herokuapp.com';
+// export const url = 'http://localhost:3000';
 
 export const store = require('store');
 
