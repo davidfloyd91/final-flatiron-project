5d901f3cc8aa72caa67e481071fcfcebe261c09d
davidfloyd91 <davidfloyd91@gmail.com>
2019-04-29 18:06:19 -0400

    removed react and react dom script tags

--- src/EmbedCode.js
+++ src/EmbedCode.js
@@ -32,8 +32,10 @@ const EmbedCode = props => {
     document.execCommand('copy');
   };
 
+  // you removed react and react-dom below: <script src='https://unpkg.com/react@16/umd/react.development.js' crossorigin></script><script src='https://unpkg.com/react-dom@16/umd/react-dom.development.js' crossorigin></script>
+
   if (data()) {
-    const copyData = `<iframe srcdoc="<div id='embed_container'></div><script src='https://unpkg.com/react@16/umd/react.development.js' crossorigin></script><script src='https://unpkg.com/react-dom@16/umd/react-dom.development.js' crossorigin></script><script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js' crossorigin></script><canvas id='salp-chart' width='740px' height='540px'></canvas><script>new Chart(document.getElementById('salp-chart'),${embedCode});</script>" width="740px" height="545px"></iframe>`
+    const copyData = `<iframe srcdoc="<div id='embed_container'></div><script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js' crossorigin></script><canvas id='salp-chart' width='740px' height='540px'></canvas><script>new Chart(document.getElementById('salp-chart'),${embedCode});</script>" width="740px" height="545px"></iframe>`
 
     return (
       <Fragment>
