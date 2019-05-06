df5e692ac5373462d3995d700a0d10ddd4babe5d
davidfloyd91 <davidfloyd91@gmail.com>
2019-05-06 14:53:37 -0400

    fix url in helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-// export const url = 'https://salp-api.herokuapp.com';
+export const url = 'https://salp-api.herokuapp.com';
 export const url = 'http://localhost:3000';
 
 export const store = require('store');
