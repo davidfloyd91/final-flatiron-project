6e926582a9c0fd6e253919399faed1b3d04baffd
davidfloyd91 <davidfloyd91@gmail.com>
2019-05-31 13:19:39 -0400

    reverse server.js changes, set homepage in package.json

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-
-// https://www.reddit.com/r/webdev/comments/ai5ul2/heroku_shows_a_blank_page_when_i_go_to_my_website/eemqj97?utm_source=share&utm_medium=web2x
-// app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/client/build'));
-
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
   return res.send('pong');
