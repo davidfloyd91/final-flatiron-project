c1a86e23dc379e66237c49caab092eb1dff43521
davidfloyd91 <davidfloyd91@gmail.com>
2019-05-31 12:55:21 -0400

    production build

---
+++ build/precache-manifest.9734536a780e49a88748c7c0ad980b33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "894802f55798d60ee1fd",
+    "url": "/davidfloyd91/Salp/static/css/main.0e6cb702.chunk.css"
+  },
+  {
+    "revision": "894802f55798d60ee1fd",
+    "url": "/davidfloyd91/Salp/static/js/main.98e11d5b.chunk.js"
+  },
+  {
+    "revision": "0da0ab93f99a059b36eb",
+    "url": "/davidfloyd91/Salp/static/js/runtime~main.c67880b1.js"
+  },
+  {
+    "revision": "992aa57072beff5a9228",
+    "url": "/davidfloyd91/Salp/static/js/2.e1df358c.chunk.js"
+  },
+  {
+    "revision": "8b6ae53901f7da6807cafccb84fcaa14",
+    "url": "/davidfloyd91/Salp/index.html"
+  }
+];
